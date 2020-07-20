536d9f47196a9d988d31ef9701bf8dd3090f6f16
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-20 11:14:38 +0200

    add __about__, __init__

---
+++ autotiling/__about__.py
@@ -0,0 +1,10 @@
+try:
+    # Python 3.8
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+try:
+    __version__ = metadata.version("autotiling")
+except Exception:
+    __version__ = "unknown"
---
+++ autotiling/__init__.py
@@ -0,0 +1,4 @@
+from main import main
+from __about__ import __version__
+
+__all__ = ["main", "__version__"]
